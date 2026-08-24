4bb89467cde6ad9d4b5f0b17d062b95132a1544a
amjadnaveed172-afk <amjadnaveed172@gmail.com>
2026-08-24 18:27:08 +0500

    Add .gitignore, remove build artifacts from tracking

--- .next/dev/static/chunks/web_0pvlx7f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1w90qxm._.css","static/chunks/0aq__next_dist_1nfclwq._.js","static/chunks/0aq__@supabase_auth-js_dist_module_0-v2xn-._.js","static/chunks/0aq__151zr7n._.js","static/chunks/web_0mmxq0w._.js","static/chunks/web_0c-u4pt._.js","static/chunks/0aq__0z4zkzc._.js"],
-    source: "entry"
-});
